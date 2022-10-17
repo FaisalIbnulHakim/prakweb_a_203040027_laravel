e1f9129191109b3d53369317354cce5fea9af371
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-10-17 08:00:16 +0700

    menyelesaikan video 3 (struktur folder, routes & view)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Faisal Ibnul Hakim",
+        "email" => "faisalhakim081@gmail.com",
+        "image" => "fiuls.png"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
