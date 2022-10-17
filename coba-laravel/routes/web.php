95098330df9d3121b445ddeec2f859b8d4411b94
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-10-17 08:28:50 +0700

    menyelesaikan video 4 (blade templating engine)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,16 +14,63 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "name" => "Faisal Ibnul Hakim",
         "email" => "faisalhakim081@gmail.com",
-        "image" => "fiuls.png"
+        "image" => "fiuls.png",
+        "title" => "About"
     ]);
 });
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Fiuls",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Icong",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum? fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
+        ]
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Fiuls",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Icong",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum? fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
+        ]
+    ];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
