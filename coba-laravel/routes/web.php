3b81a13d0c47f7f5b4e58d503ca534ca0aa467ee
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-10-17 08:49:33 +0700

    menyelesaikan video 5 (model, collection & controller)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,49 +30,6 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fiuls",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Icong",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum? fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
-        ]
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fiuls",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Icong",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum? fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
-        ]
-    ];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
