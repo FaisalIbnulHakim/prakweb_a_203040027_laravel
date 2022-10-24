1604e14f0e2952f8e25735957766c12d031159fb
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-10-24 14:43:23 +0700

    menyelesaikan video 14 (view login & registration)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
-use App\Models\Post;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -61,3 +62,6 @@ Route::get('categories', function () {
 //         'posts' => $author->posts->load('category', 'author'),
 //     ]);
 // });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
