8b9662d3d3fa5d033c6b03d4722881364865018c
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-11-19 20:00:12 +0700

    menyelesaikan video 22 (preview, update & delete image)

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use illuminate\Support\Str;
 
 
@@ -51,12 +52,12 @@ class DashboardPostController extends Controller
             'image' => 'image|file|max:1024',
             'body' => 'required'
         ]);
-        $validateData['user_id'] = auth()->user()->id;
-        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
-
         if ($request->file('image')) {
             $validateData['image'] = $request->file('image')->store('post-images');
         }
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
         Post::create($validateData);
 
         return redirect('/dashboard/posts')->with('success', 'New Post has been added!');
@@ -102,6 +103,7 @@ class DashboardPostController extends Controller
             'title' => 'required|max:255',
             'category_id' => 'required',
             'slug' => 'required|unique:posts',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ]);
 
@@ -109,7 +111,12 @@ class DashboardPostController extends Controller
             $rules['slug'] = 'required|unique:posts';
         }
         $validateData = $request->validate($rules);
-
+        if ($request->file('image')) {
+            if ($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validateData['image'] = $request->file('image')->store('post-images');
+        }
         $validateData['user_id'] = auth()->user()->id;
         $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
@@ -127,6 +134,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
